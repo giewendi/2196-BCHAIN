dc7ce3d84cc91246e771ff4c5e2e739aa22bfd06
Giewen Pinlac <giewenpinlac@gmail.com>
2024-04-20 17:31:48 +0800

    add smart contract

---
+++ Smart Contract/scripts/interact.js
@@ -0,0 +1,31 @@
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+//For Hardhat
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+
+console.log(JSON.stringify(contract.abi));
+
+//interact.js
+const ethers = require('ethers');
+
+//provider
+const alchemyProvider = new ethers.providers.JsonRpcProvider(API_URL);
+
+//signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+//contract
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main(){
+    const message = await helloWorldContract.message();
+    console.log("The message is: " + message);
+
+    console.log("Updating the message...");
+    const tx = await helloWorldContract.update("This is the new message.");
+    await tx.wait();
+}
+
+main();
