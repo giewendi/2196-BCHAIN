61ef3cf9b5ae9b540035d5f593d3d23845bb7fc4
Giewen Pinlac <giewenpinlac@gmail.com>
2024-04-21 03:27:56 +0800

    Updated Finals Lab 5 Files

---
+++ FinalsLab5/starter-files/src/util/interact.js
@@ -0,0 +1,145 @@
+const alchemyKey = "wss://eth-sepolia.g.alchemy.com/v2/4nekGikenqR6KRi2Tz5nGUnA740k3OmF"
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey); 
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0xfc4518cA5770960BdF5c66Cd4D8e6A46EaE695da";
+
+export const helloWorldContract = new web3.eth.Contract(
+    contractABI,
+    contractAddress
+  );
+
+//export const helloWorldContract;
+
+export const loadCurrentMessage = async () => {
+  const message = await helloWorldContract.methods.message().call();
+  return message;
+};
+
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        status: "☝️ Write a message in the text-field above.",
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a targer="_blank" href={"https://metamask.io/download"}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "Write a message in the text-field above.",
+        };
+      } else {
+        return {
+          address: "",
+          status: "Connect to Metamask using the top right button.",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {""}
+            🦊{""}
+            <a target="_blank" href={"https://metamask.io/download"}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const updateMessage = async (address, message) => {
+  if (!window.ethereum || address === null) {
+    return {
+      status:
+        "Connect your Metamask wallet to update the message on the blockchain.",
+    };
+  }
+  if (message.trim() === "") {
+    return {
+      status: "Your message cannot be an empty string.",
+    };
+  }
+
+  //set up transaction parameters
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: address, // must match user's active address.
+    data: helloWorldContract.methods.update(message).encodeABI(),
+  };
+
+  //sign the transaction
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      status: (
+        <span>
+          ✓{" "}
+          <a
+            target="_blank"
+            href={"https://sepolia.ethers can.io/tx/${txHash}"}
+          >
+            {" "}
+            View the status of your transaction on Etherscan!
+          </a>
+          <br />
+          Once the transaction is verified by the network, the message will be
+          updated automatically.
+        </span>
+      ),
+    };
+  } catch (error) {
+    return {
+      status: " " + error.message,
+    };
+  }
+};
